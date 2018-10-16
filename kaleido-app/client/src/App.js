3ff4d34215a06d6f7d351cb74528be3b9e2c6f11
Anastasia Lalamentik <lanasta@live.unc.edu>
2018-10-16 12:46:38 -0400

    Setup SCSS

--- kaleido-app/client/src/App.js
+++ kaleido-app/client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import logo from './logo.svg';
 
+import './App.scss'; // or `.scss` if you chose scss
 import './App.min.css';
 
 class App extends Component {
